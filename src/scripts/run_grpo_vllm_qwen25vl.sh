a47298feb933fb7f84f0e59e4407d7fe3570ce07
LeeYu <brightlight720720@gmail.com>
2025-06-27 21:52:18 +0800

    Update run_grpo_vllm_qwen25vl.sh

--- src/scripts/run_grpo_vllm_qwen25vl.sh
+++ src/scripts/run_grpo_vllm_qwen25vl.sh
@@ -12,7 +12,7 @@ export DEBUG_MODE="true"
 export LOG_PATH="./vllm_run.txt"
 
 QWEN_PATH="/workspace/models/Qwen2.5-VL-3B-Instruct"
-HF_DATASET="Leeyuyu/fundo"
+HF_DATASET="Leeyuyu/fundo_400"
 OUTPUT_DIR="/workspace/outputs/grpo_run"
 if [ ! -d "$OUTPUT_DIR" ]; then
  mkdir -p "$OUTPUT_DIR"
@@ -33,20 +33,24 @@ CUDA_VISIBLE_DEVICES="0,1,2,3" torchrun \
     --node_rank="0" \
     --master_addr="127.0.0.1" \
     --master_port="12345" \
-    /workspace/R1-V_fundo/src/r1-v/src/open_r1/grpo.py \
+    /workspace/R1-V_fundo/src/r1-v/src/open_r1/grpo_25.py \
     --use_vllm true \
     --output_dir ${OUTPUT_DIR} \
     --model_name_or_path ${QWEN_PATH} \
     --dataset_name ${HF_DATASET} \
     --max_prompt_length 4096 \
     --max_completion_length 2048 \
-    --per_device_train_batch_size 2 \
-    --gradient_accumulation_steps 4 \
-    --learning_rate 1e-6 \
-    --lr_scheduler_type "constant" \
+    --learning_rate 5e-7 \
+    --lr_scheduler_type cosine \
+    --warmup_steps 60 \
+    --weight_decay 0.03 \
     --logging_steps 1 \
-    --bf16 true \
     --gradient_checkpointing true \
+    --attn_implementation flash_attention_2 \
+    --max_steps 2000 \
+    --gradient_accumulation_steps 8 \
+    --per_device_train_batch_size 2 \
+    --bf16 true \
     --attn_implementation flash_attention_2 \
     --min_pixels 3136 \
     --max_pixels 501760 \
@@ -54,12 +58,11 @@ CUDA_VISIBLE_DEVICES="0,1,2,3" torchrun \
     --run_name ${RUN_NAME} \
     --save_steps 200 \
     --save_total_limit 3 \
-    --save_only_model true \
     --report_to wandb \
     --temperature 1.0 \
     --num_generations 4 \
     --vllm_device "cuda:3" \
-    --vllm_gpu_memory_utilization 0.9 \
+    --vllm_gpu_memory_utilization 0.8 \
     --deepspeed ${DS_CONFIG} \
     2>&1 | tee "${OUTPUT_DIR}/training_log.txt"
 
